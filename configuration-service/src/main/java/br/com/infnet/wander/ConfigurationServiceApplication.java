08f2088ed7a1186d13c660590b1a020e9bd8b8dd
wanderfalcao <33440362+wanderfalcao@users.noreply.github.com>
2023-04-01 16:44:01 -0300

    discovery and config service
    adding Dockerfile.
    Giving responsability of the configuration to the config service

---
+++ configuration-service/src/main/java/br/com/infnet/wander/ConfigurationServiceApplication.java
@@ -0,0 +1,15 @@
+package br.com.infnet.wander;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.config.server.EnableConfigServer;
+
+@SpringBootApplication
+@EnableConfigServer
+public class ConfigurationServiceApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ConfigurationServiceApplication.class, args);
+    }
+
+}
